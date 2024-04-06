a93b5d60b867aca5d940761c124ff6736c1ae634
Malfrador <malfrador@gmail.com>
2024-04-06 21:43:36 +0200

    Add more attributes, load attributes from data asset

--- Source/Hunt/Public/AHuntCharacter.h
+++ Source/Hunt/Public/AHuntCharacter.h
@@ -28,5 +28,6 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
 
 };
--- Source/Hunt/Public/UHuntAttributeSet.h
+++ Source/Hunt/Public/UHuntAttributeSet.h
@@ -18,17 +18,22 @@ class UHuntAttributeSet : public UAttributeSet
 public:
  
 	// Current health of the owner
-	UPROPERTY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FGameplayAttributeData Health = 100.0f;
 	ATTRIBUTE_ACCESSORS(UHuntAttributeSet, Health)
  
 	// Max health that the owner can have
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FGameplayAttributeData MaxHealth= 100.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData MaxHealth= 500.0f;
 	ATTRIBUTE_ACCESSORS(UHuntAttributeSet, MaxHealth)
 
 	// Damage the owner can deal with one attack
-	UPROPERTY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FGameplayAttributeData Damage = 90.0f;
 	ATTRIBUTE_ACCESSORS(UHuntAttributeSet, Damage)
+
+	// Movement speed
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData MovementSpeed = 5.0f;
+	ATTRIBUTE_ACCESSORS(UHuntAttributeSet, MovementSpeed)
 };
