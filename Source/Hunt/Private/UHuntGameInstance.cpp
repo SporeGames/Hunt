54ab9dac22d8a531c6a9a083b6a39151914c8c39
Malfrador <malfrador@gmail.com>
2024-05-16 02:19:32 +0200

    Add loading screen

---
+++ Source/Hunt/Private/UHuntGameInstance.cpp
@@ -0,0 +1,27 @@
+﻿#include "UHuntGameInstance.h"
+#include "MoviePlayer.h"
+
+void UHuntGameInstance::Init()
+{
+	Super::Init();
+
+	FCoreUObjectDelegates::PreLoadMap.AddUObject(this, &UHuntGameInstance::BeginLoadingScreen);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UHuntGameInstance::EndLoadingScreen);
+}
+
+void UHuntGameInstance::BeginLoadingScreen(const FString& InMapName)
+{
+	if (!IsRunningDedicatedServer())
+	{
+		FLoadingScreenAttributes LoadingScreen;
+		LoadingScreen.bAutoCompleteWhenLoadingCompletes = true;
+		LoadingScreen.WidgetLoadingScreen = FLoadingScreenAttributes::NewTestLoadingScreenWidget();
+ 
+		GetMoviePlayer()->SetupLoadingScreen(LoadingScreen);
+	}
+}
+
+void UHuntGameInstance::EndLoadingScreen(UWorld* InLoadedWorld)
+{
+
+}
---
+++ Source/Hunt/Public/UHuntGameInstance.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "UHuntGameInstance.generated.h"
+UCLASS()
+class UHuntGameInstance : public UGameInstance {
+	GENERATED_BODY()
+public:
+	virtual void Init() override;
+
+	UFUNCTION()
+	virtual void BeginLoadingScreen(const FString& MapName);
+	UFUNCTION()
+	virtual void EndLoadingScreen(UWorld* InLoadedWorld);
+};
